118e09b20dad1883cbbab02865c6b211d1a33c66
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2024-06-03 11:25:20 -0400

    Create Mortgage.kt

---
+++ Mortgage.kt
@@ -0,0 +1,71 @@
+
+import android.annotation.SuppressLint
+import android.util.Log
+import kotlin.text.*
+
+class Mortgage(years: Int, amount: Double, interestRate: Double) {
+
+    private var years: Int = 0
+    private var amount: Double = 0.0
+    private var interestRate: Double = 0.0
+
+    init {
+        Log.w("MainActivity", "Inside init")
+    }
+
+    init {
+
+        setYears(years)
+        setAmount(amount)
+        setInterestRate(interestRate)
+    }
+
+    fun getYears(): Int {
+        return years
+    }
+
+    fun getAmount(): Double {
+        return amount
+    }
+
+    fun getInterestRate(): Double {
+        return interestRate
+    }
+
+    fun setInterestRate(interestRate: Double) {
+        this.interestRate = interestRate
+    }
+
+    fun setYears(years: Int) {
+        this.years = years
+    }
+
+    fun setAmount(amount: Double) {
+        this.amount = amount
+    }
+
+    private fun monthlyPayment(): Double {
+
+        var yearlyPayment = this.amount / this.years
+        val yearlyWithInterest = yearlyPayment * this.interestRate
+        yearlyPayment += yearlyWithInterest
+        val monthlyPayment = yearlyPayment / 12.0
+        return monthlyPayment
+
+    }
+
+    private fun yearlyPayments(): Double {
+        return monthlyPayment() * 12
+    }
+
+    @SuppressLint("DefaultLocale")
+    fun formattedOutput(): String{
+        return String.format("%.2f", monthlyPayment())
+    }
+
+    @SuppressLint("DefaultLocale")
+    fun formattedOutputYear(): String{
+        return String.format("%.2f", yearlyPayments())
+    }
+
+}
