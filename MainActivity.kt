99b01896228faec0f1f216a5d57cdba9ec85d018
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2024-06-03 23:56:21 -0400

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -14,24 +14,24 @@ class MainActivity : ComponentActivity() {
         val mortgage = Mortgage(30, 300000.0, 0.065)
 
         println("Mortgage Details: ")
-        println("Monthly Payments: ${mortgage.formattedOutput()}")
-        println("Yearly Payments: ${mortgage.formattedOutputYear()}")
+        println("Monthly Payments: $${mortgage.formattedOutput()}")
+        println("Yearly Payments: $${mortgage.formattedOutputYear()}")
 
         Log.d("MainActivity", "Mortgage Details: ")
-        Log.d("MainActivity", "Monthly Payments: ${mortgage.formattedOutput()}")
-        Log.d("MainActivity", "Yearly Payments: ${mortgage.formattedOutputYear()}")
+        Log.d("MainActivity", "Monthly Payments: $${mortgage.formattedOutput()}")
+        Log.d("MainActivity", "Yearly Payments: $${mortgage.formattedOutputYear()}")
 
         mortgage.setYears(15)
         mortgage.setAmount(100000.00)
         mortgage.setInterestRate(0.07)
 
         println("Updated Mortgage Details: ")
-        println("Monthly Payments: ${mortgage.formattedOutput()}")
-        println("Yearly Payments: ${mortgage.formattedOutputYear()}")
+        println("Monthly Payments: $${mortgage.formattedOutput()}")
+        println("Yearly Payments: $${mortgage.formattedOutputYear()}")
 
         Log.d("MainActivity", "Updated Mortgage Details: ")
-        Log.d("MainActivity", "Monthly Payments: ${mortgage.formattedOutput()}")
-        Log.d("MainActivity", "Yearly Payments: ${mortgage.formattedOutputYear()}")
+        Log.d("MainActivity", "Monthly Payments: $${mortgage.formattedOutput()}")
+        Log.d("MainActivity", "Yearly Payments: $${mortgage.formattedOutputYear()}")
 
     }
 
